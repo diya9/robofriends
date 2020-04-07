b4defd5cd679d2c3e64b1fbec4b3c977639c86fc
diya9 <pallavibhosale2012@gmail.com>
2020-04-07 16:51:18 +0530

    Updated Package

--- src/robots.js
+++ src/robots.js
@@ -1,44 +1,44 @@
 export const robots = [{
         id: 1,
-        name: 'Leanne Graham',
-        username: 'Bret',
-        email: 'Sincere@april.biz'
+        name: 'Narendra Bagul',
+        username: 'crypton',
+        email: 'naruModi@april.biz'
     },
     {
         id: 2,
-        name: 'Ervin Howell',
-        username: 'Antonette',
-        email: 'Shanna@melissa.tv'
+        name: 'Kirtiraj ',
+        username: 'Kirti',
+        email: 'kieti2019@melissa.tv'
     },
     {
         id: 3,
-        name: 'Clementine Bauch',
-        username: 'Samantha',
-        email: 'Nathan@yesenia.net'
+        name: 'Shuhbam Wange',
+        username: 'shubhya',
+        email: 'photography@yesenia.net'
     },
     {
         id: 4,
-        name: 'Patricia Lebsack',
-        username: 'Karianne',
-        email: 'Julianne.OConner@kory.org'
+        name: 'Rajani Suryawanshi',
+        username: 'rajjo',
+        email: 'radkiRajjo@kory.org'
     },
     {
         id: 5,
-        name: 'Chelsey Dietrich',
-        username: 'Kamren',
-        email: 'Lucio_Hettinger@annie.ca'
+        name: 'Amol Funde',
+        username: 'amu',
+        email: 'amol_Funde@annie.ca'
     },
     {
         id: 6,
-        name: 'Mrs. Dennis Schulist',
-        username: 'Leopoldo_Corkery',
-        email: 'Karley_Dach@jasper.info'
+        name: 'Sangram Pawar',
+        username: 'pawarSaheb',
+        email: 'sanguMan@jasper.info'
     },
     {
         id: 7,
-        name: 'Kurtis Weissnat',
-        username: 'Elwyn.Skiles',
-        email: 'Telly.Hoeger@billy.biz'
+        name: 'Priyanka',
+        username: 'sahu',
+        email: 'Great_Mom@billy.biz'
     },
     {
         id: 8,
