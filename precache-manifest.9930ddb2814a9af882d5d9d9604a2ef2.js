14a15ad278d34213f21a7720e159688e1a499b65
diya9 <pallavibhosale2012@gmail.com>
2020-04-07 18:10:08 +0530

    Updates

---
+++ precache-manifest.9930ddb2814a9af882d5d9d9604a2ef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9cd8b28fb6021cfe617545b19772ad",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3f6f711d38b3941ecb08",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "6d6764dcfdf990d385d6",
+    "url": "/robofriends/static/css/main.34666554.chunk.css"
+  },
+  {
+    "revision": "3f6f711d38b3941ecb08",
+    "url": "/robofriends/static/js/2.f5f94b4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends/static/js/2.f5f94b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6764dcfdf990d385d6",
+    "url": "/robofriends/static/js/main.3010e8ab.chunk.js"
+  },
+  {
+    "revision": "dab07940c1980e5aab1e",
+    "url": "/robofriends/static/js/runtime-main.a4afcfec.js"
+  }
+]);
